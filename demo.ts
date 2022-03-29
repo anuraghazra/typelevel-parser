df42c3ae2f9b914dfe044576f52410a03bcc64b7
Anurag <hazru.anurag@gmail.com>
2022-03-29 21:04:16 +0530

    chore: added demo

--- demo.ts
+++ demo.ts
@@ -1,24 +1,60 @@
+import { PathAutocomplete } from "./autocomplete";
 import { Interpret } from "./interpreter";
 import { Parser } from "./parser";
 import { Tokenize } from "./tokenizer";
 
-type Run<T extends string> = Interpret<{}, Parser<Tokenize<T>>>
+type Run<Obj, T extends PathAutocomplete<Obj>> = Interpret<
+  Obj,
+  Parser<Tokenize<T & string>>
+>;
 
-type Demo1 = Run<'a.b.c'>;
-//   ^? "Unexpected end of input, expected IDENT"
-type Demo2 = Run<'a.b['>;
-//   ^? "Unexpected end of input, Expected token of type BRACKET_END"
-type Demo3 = Run<'a.b]'>;
-//   ^? "Unexpected token BRACKET_END"
-type Demo4 = Run<'a.b[0'>;
-//   ^? "Unexpected end of input, Expected token of type BRACKET_END"
-type Demo5 = Run<'a.b[0]]'>;
-//   ^? "Unexpected token BRACKET_END"
-type Demo6 = Run<'a.b[0].'>;
-//   ^? "Unexpected end of input, expected IDENT"
-type Demo7 = Run<'a.b[0].['>;
-//   ^? "Expected token of type IDENT, got BRACKET_START"
-type Demo8 = Run<'a.b[0].]'>;
-//   ^? "Expected token of type IDENT, got BRACKET_END"
-type Demo9 = Run<'a.b[0]..'>;
-//   ^? "Expected token of type IDENT, got DOT"
+const obj = {
+  commentsssssssssssssssss: {
+    nest: {
+      child: {
+        nest: {
+          child: {
+            nest: {
+              child: {
+                nest: {
+                  child: {
+                    nest: {
+                      child: {
+                        nest: {
+                          child: {
+                            nest: {
+                              child: {
+                                nest: {
+                                  child: {
+                                    nest: {
+                                      child: {
+                                        nest: {
+                                          child: [1, 2, 3],
+                                        },
+                                      },
+                                    },
+                                  },
+                                },
+                              },
+                            },
+                          },
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+} as const;
+
+// It's soooo big I can't even show the whole thing.
+
+type Demo = Run<
+  typeof obj,
+  "commentsssssssssssssssss.nest.child.nest.child.nest.child.nest.child.nest.child.nest.child.nest.child.nest.child.nest.child.nest.child[]"
+>;
